de36ee071c3b231df291fab6fbefa481d73ce0ba
victorgrcp <victorgrcp@gmail.com>
2023-03-03 12:38:41 +0100

    impl loops

--- chomp-game/src/main.rs
+++ chomp-game/src/main.rs
@@ -2,14 +2,36 @@
 use game::*;
 mod game;
 
+fn get_move() -> (usize, usize) {
+    let mut n = String::new();
+    let mut m = String::new();
+    println!("Enter row and column: ");
+    std::io::stdin().read_line(&mut n).expect("Failed to read line");
+    std::io::stdin().read_line(&mut m).expect("Failed to read line");
+    let n: usize = n.trim().parse().expect("Please type a number!");
+    let m: usize = m.trim().parse().expect("Please type a number!");
+    (n, m)
+}
+
 fn main() {
     let mut game = Game::new(10, 10);
-    let (n, m) = (9, 0);
-    let step: Move = game.fill_board(n, m);
-    match step {
-        Move::Valid => println!("Valid move!"),
-        Move::Invalid => println!("Invalid move!"),
-        Move::Final => println!("Final move!"),
+    let mut n: usize;
+    let mut m: usize;
+    let mut step: Move;
+    loop {
+        game.show_board();
+        loop { // Loop for invalid move
+            (n, m) = get_move();
+            step = game.fill_board(n, m);
+            if step != Move::Invalid { break; };
+            println!("Invalid move!");
+        }
+        match step {
+            Move::Valid => println!("\n[*] Valid move! \n"),
+            Move::Final => println!("\n[*] Final move! \n"),
+            _ => (),
+        }
+        if step == Move::Final { break; }
     }
     game.show_board();
 }
